ce2896a32a9e13c0f298e7ab198aa8b0dbcca9c3
motomatt5040 <motomatt5040@gmail.com>
2024-06-27 21:05:47 -0500

    feat(database.py): add random message puller
    A random message puller has been added to the database.py file to grab a random previoulsy posted or edited message.
    
    BREAKING CHANGE: It is possible that the random index may populate incorrectly, this is very unlikely, but if the command happens in the middle of an upload it may occur.

--- database.py
+++ database.py
@@ -1,6 +1,5 @@
 import sqlite3
-import discord
-import traceback
+import random
 
 class Database:
     def __init__(self):
@@ -16,3 +15,21 @@ class Database:
         sql = f"INSERT INTO tbl{server_id}(message_id, content, rec_date, attachment, embed) VALUES (?, ?, ?, ?, ?)"
         self.cur.execute(sql, parameters)
         self.conn.commit()
+
+    def get_random_message(self, server_id: str = None):
+        if server_id is None:
+            server_id = 'Skin_Walkers'
+        sql = f"SELECT * FROM tbl{server_id}"
+        exe = self.cur.execute(sql)
+        data = exe.fetchall()
+
+        random_selection = data[random.randint(0, len(data) - 1)]
+        message = {
+            "message_id": random_selection[0],
+            "content": random_selection[1],
+            "rec_date": random_selection[2],
+            "attachment": random_selection[3],
+            "embed": random_selection[4]
+        }
+
+        return message
